8ce2521b8c0a6fac8fda87596994bd04b70a064b
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-05-14 21:33:54 -0300

    initial scaffolding and load gifs with gif api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import getGifs from "./services/getGifs";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      getGifs().then((gifs) => setGifs(gifs));
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>Gifs</h1>
+        {gifs &&
+          gifs.map(({ id, title, url }) => (
+            <div> 
+              <h4>{title}</h4>
+              <small>{id}</small>
+              <img src={url} alt={title} key={id} />
+            </div>
+          ))}
+      </section>
     </div>
   );
 }
