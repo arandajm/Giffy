ae65dab832840b69454a72b27b90e2ee9a00dfce
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-05-15 10:16:24 -0300

    add routing using wrouter and loading animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
+import { Link, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
         <h1>Gifs</h1>
-        <ListOfGifs keyword="argentina"/>
+        <Link to="/gif/artists">Artists
+        </Link>
+        <Link to="/gif/locos">
+          Locos
+        </Link>
+        <Route path="/gif/:keyword" component={ListOfGifs} />
       </section>
     </div>
   );
